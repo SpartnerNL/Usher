7ada3dd0765c07791855753ebbc471db7daaf8ba
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2015-05-03 19:15:23 +0200

    Eager load roles when fetching user

--- src/Infrastructure/Users/DoctrineUserRepository.php
+++ src/Infrastructure/Users/DoctrineUserRepository.php
@@ -1,16 +1,11 @@
 <?php namespace Maatwebsite\Usher\Infrastructure\Users;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Mapping\ClassMetadata;
-use Doctrine\ORM\EntityManagerInterface;
-use Maatwebsite\Usher\Contracts\Users\User;
-use Maatwebsite\Usher\Domain\Users\Embeddables\Name;
-use Maatwebsite\Usher\Domain\Users\Embeddables\Email;
-use Maatwebsite\Usher\Contracts\Users\UserRepository;
-use Maatwebsite\Usher\Domain\Users\Embeddables\Password;
 use Maatwebsite\Usher\Contracts\Users\Embeddables\Email as EmailInterface;
-use Maatwebsite\Usher\Domain\Users\Events\UserRegistered;
-use Maatwebsite\Usher\Domain\Users\Events\UserUpdatedProfile;
+use Maatwebsite\Usher\Contracts\Users\User;
+use Maatwebsite\Usher\Contracts\Users\UserRepository;
 
 class DoctrineUserRepository extends EntityRepository implements UserRepository
 {
@@ -54,7 +49,9 @@ class DoctrineUserRepository extends EntityRepository implements UserRepository
 
     /**
      * Find user by email
+     *
      * @param EmailInterface $email
+     *
      * @return mixed
      */
     public function findByEmail(EmailInterface $email)
@@ -66,7 +63,9 @@ class DoctrineUserRepository extends EntityRepository implements UserRepository
 
     /**
      * Find a user by its credentials
+     *
      * @param  array $credentials
+     *
      * @return mixed
      */
     public function findByCredentials(array $credentials)
@@ -83,12 +82,69 @@ class DoctrineUserRepository extends EntityRepository implements UserRepository
             }
         }
 
-        return $this->findOneBy($creds);
+        $builder = $this->createQueryBuilder('u');
+        $builder->select('u', 'r');
+        $builder->join('u.roles', 'r');
+
+        foreach ($creds as $name => $value) {
+            $builder->where('u. ' + $name + ' = :' . $name);
+        }
+
+        $query = $builder->getQuery();
+        foreach ($creds as $name => $value) {
+            $query->setParameter($name, $value);
+        }
+
+        return $query->getOneOrNullResult();
+    }
+
+    /**
+     * @param $id
+     *
+     * @return User
+     */
+    public function findWithRole($id)
+    {
+        $builder = $this->createQueryBuilder('u');
+        $builder->select('u', 'r');
+        $builder->join('u.roles', 'r');
+
+        $builder->where('u.id = :id');
+
+        $query = $builder->getQuery();
+        $query->setParameter('id', $id);
+
+        return $query->getSingleResult();
+    }
+
+    /**
+     * @param $criteria
+     *
+     * @return User
+     */
+    public function findWithRoleByCriteria($criteria)
+    {
+        $builder = $this->createQueryBuilder('u');
+        $builder->select('u', 'r');
+        $builder->join('u.roles', 'r');
+
+        foreach ($criteria as $name => $value) {
+            $builder->where('u. ' + $name + ' = :' . $name);
+        }
+
+        $query = $builder->getQuery();
+        foreach ($criteria as $name => $value) {
+            $query->setParameter($name, $value);
+        }
+
+        return $query->getOneOrNullResult();
     }
 
     /**
      * Persist entity
+     *
      * @param $entity
+     *
      * @return mixed
      */
     public function persist($entity)
@@ -107,6 +163,7 @@ class DoctrineUserRepository extends EntityRepository implements UserRepository
 
     /**
      * Deletes a user
+     *
      * @param User $user
      */
     public function delete(User $user)
