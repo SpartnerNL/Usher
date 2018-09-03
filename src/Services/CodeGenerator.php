8aa702bb483a062f2b2b9cd915a1bd077a6b57ba
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2018-09-03 12:42:11 +0200

    Remove mcrypt

--- src/Services/CodeGenerator.php
+++ src/Services/CodeGenerator.php
@@ -2,44 +2,11 @@
 
 class CodeGenerator
 {
-
-    /**
-     * @var string
-     */
-    private static $hashing = 'sha256';
-
     /**
      * @return string
      */
     public static function make()
     {
-        return hash(static::$hashing, self::generateEntropy());
-    }
-
-    /**
-     * @return string
-     */
-    public static function generateEntropy()
-    {
-        $entropy = mcrypt_create_iv(32, self::getRandomizer());
-        $entropy .= uniqid(mt_rand(), true);
-
-        return $entropy;
-    }
-
-    /**
-     * @return int
-     */
-    protected static function getRandomizer()
-    {
-        if (defined('MCRYPT_DEV_URANDOM')) {
-            return MCRYPT_DEV_URANDOM;
-        }
-        if (defined('MCRYPT_DEV_RANDOM')) {
-            return MCRYPT_DEV_RANDOM;
-        }
-        mt_srand();
-
-        return MCRYPT_RAND;
+        return str_random(32);
     }
 }
